7c91adf088d42e8459dd6ab5810a0033ea03a715
Georgi Bozhinov <ge.bozhinov@gmail.com>
2025-07-20 16:53:11 +0300

    add secutiry config

---
+++ src/main/java/bg/coincraft/userservice/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package bg.coincraft.userservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
--- src/main/java/bg/coincraft/userservice/service/UserServiceImpl.java
+++ src/main/java/bg/coincraft/userservice/service/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package bg.coincraft.userservice.service;
 
+import bg.coincraft.userservice.exception.ConstraintViolationException;
 import bg.coincraft.userservice.model.CreateUserDTO;
 import bg.coincraft.userservice.model.db.UserEntity;
 import bg.coincraft.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.authentication.password.CompromisedPasswordDecision;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,9 +17,15 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordService passwordService;
+    private final CompromisedPasswordChecker compromisedPasswordChecker;
 
     @Override
     public UserEntity create(CreateUserDTO createUserDTO) {
+        CompromisedPasswordDecision checkedPassword = compromisedPasswordChecker.check(createUserDTO.getPassword());
+        if (checkedPassword.isCompromised()) {
+            throw new ConstraintViolationException("Password compromised");
+        }
+
         UserEntity user = UserEntity.builder()
                 .setUsername(createUserDTO.getUsername())
                 .setPassword(passwordService.encrypt(createUserDTO.getPassword()))
