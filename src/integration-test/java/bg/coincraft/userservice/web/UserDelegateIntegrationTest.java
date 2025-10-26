b5170190964155b30891272bdb58e8d78fd8cdc5
Georgi Bozhinov <ge.bozhinov@gmail.com>
2025-10-26 10:00:03 +0200

    use MockitoBean inseat of Mock

--- src/integration-test/java/bg/coincraft/userservice/web/UserDelegateIntegrationTest.java
+++ src/integration-test/java/bg/coincraft/userservice/web/UserDelegateIntegrationTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -21,7 +22,7 @@ public class UserDelegateIntegrationTest extends IntegrationTestInit {
     private UserDelegate userDelegate;
     @Autowired
     private EntityManager entityManager;
-    @Mock
+    @MockitoBean
     private KeycloakService keycloakService;
 
     @Disabled
