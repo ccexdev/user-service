46b006fe65641c169ff7fe34babb0d37510bdcb7
Georgi Bozhinov <ge.bozhinov@gmail.com>
2026-02-22 13:02:32 +0200

    refactor

--- src/main/java/bg/coincraft/userservice/service/UserServiceImpl.java
+++ src/main/java/bg/coincraft/userservice/service/UserServiceImpl.java
@@ -1,31 +1,28 @@
 package bg.coincraft.userservice.service;
 
-import bg.coincraft.authenticationservice.client.model.UserDTO;
 import bg.coincraft.userservice.exception.ConstraintViolationException;
+import bg.coincraft.userservice.mapper.UserMapper;
 import bg.coincraft.userservice.model.CreateUserDTO;
 import bg.coincraft.userservice.model.db.UserEntity;
 import bg.coincraft.userservice.model.db.UserProfileEntity;
-import bg.coincraft.userservice.model.enums.GenderEnum;
-import bg.coincraft.userservice.model.enums.UserRole;
 import bg.coincraft.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.openapitools.client.api.AuthenticationApi;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.authentication.password.CompromisedPasswordDecision;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final PasswordService passwordService;
     private final CompromisedPasswordChecker compromisedPasswordChecker;
     private final AuthenticationApi authenticationApi;
 
     @Override
+    @Transactional
     public void create(CreateUserDTO createUserDTO) {
         CompromisedPasswordDecision checkedPassword = compromisedPasswordChecker.check(createUserDTO.getPassword());
         if (checkedPassword.isCompromised()) {
@@ -33,32 +30,8 @@ public class UserServiceImpl implements UserService {
         }
 
         String keycloakUserId = tryToGetKeycloakUserId(createUserDTO);
-
-        UserEntity user = UserEntity.builder()
-                .setUsername(createUserDTO.getUsername())
-                .setFirstName(createUserDTO.getFirstName())
-                .setLastName(createUserDTO.getLastName())
-                .setKeycloakId(keycloakUserId)
-                .setEmail(createUserDTO.getEmail())
-                .setIsEmailVerified(false)
-                .setPhoneNumber(createUserDTO.getPhoneNumber())
-                .setActive(false)
-                .setCreatedAt(LocalDateTime.now())
-                .setCreatedBy("SYSTEM")
-                .setRole(UserRole.USER)
-                .build();
-
-        UserProfileEntity userProfileEntity = UserProfileEntity.builder()
-                .setUserEntity(user)
-                .setCountryCode(createUserDTO.getProfile().getCountryCode())
-                .setDateOfBirth(createUserDTO.getProfile().getDateOfBirth())
-                .setGender(GenderEnum.valueOf(createUserDTO.getProfile().getGender().name()))
-                .setAddress(createUserDTO.getProfile().getAddress())
-                .setCity(createUserDTO.getProfile().getCity())
-                .setPostalCode(createUserDTO.getProfile().getPostalCode())
-                .setCountry(createUserDTO.getProfile().getCountry())
-                .build();
-
+        UserEntity user = UserMapper.toUserEntity(createUserDTO, keycloakUserId);
+        UserProfileEntity userProfileEntity = UserMapper.toUserProfilerEntity(createUserDTO, user);
         user.setUserProfileEntity(userProfileEntity);
 
         this.userRepository.save(user);
@@ -66,19 +39,9 @@ public class UserServiceImpl implements UserService {
 
     private String tryToGetKeycloakUserId(CreateUserDTO createUserDTO) {
         try {
-            return authenticationApi.register(buildUserDto(createUserDTO));
+            return authenticationApi.register(UserMapper.toKeycloakUserDto(createUserDTO));
         } catch (Exception e) {
             throw new ConstraintViolationException("User creation failed. Username: " + createUserDTO.getUsername());
         }
-    }
-
-    private UserDTO buildUserDto(CreateUserDTO createUserDTO) {
-        return UserDTO.builder()
-                .setUsername(createUserDTO.getUsername())
-                .setPassword(createUserDTO.getPassword())
-                .setEmail(createUserDTO.getEmail())
-                .setFirstName(createUserDTO.getFirstName())
-                .setLastName(createUserDTO.getLastName())
-                .build();
     }
 }
